334cb8c94589e39d7dff656a2e1954df002cb34e
Gongzai Li <gli65@uclive.ac.nz>
2021-03-27 23:22:10 +1300

    # fix space

--- app/routes/events.server.router.js
+++ app/routes/events.server.router.js
@@ -1,30 +1,16 @@
 const events = require('../controllers/events.server.controller');
 
-/**
- * 200 OK
-
- GET: The resource has been fetched and is transmitted in the message body.
- HEAD: The entity headers are in the message body.
- PUT or POST: The resource describing the result of the action is transmitted in the message body.
-
-
- 201 Created
- The request has succeeded and a new resource has been created as a result. This is typically the response sent after POST requests, or some PUT requests.
- * @param app
- */
 module.exports = function (app) {
 
     app.route(app.rootUrl + '/events/categories')//should put in the top should ask ???????????????????????????/
         .get(events.getAllCategories);
 
     app.route(app.rootUrl + '/events')
-        .get(events.readEvents)// problem
+        .get(events.readEvents)
         .post(events.createEvent);
 
     app.route(app.rootUrl + '/events/:id')
         .get(events.getOneEvent)
         .patch(events.updateEvent)
         .delete(events.deleteEvent);
-
-
 }
